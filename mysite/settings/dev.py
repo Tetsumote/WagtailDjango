5213f02cbe22bad895359d5d76b23bb3f2da51fa
Paweł Grajewski <unionproject88@gmail.com>
2021-06-13 19:58:21 +0000

    Initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pt^#4t09-=w5x+b+2ipbn79f4m&x5_0!97%ot$$pket_3g5s@('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
